7d7b239b93904f11f7ce12ed08e5d1f2b1867818
kurko <chavedomundo@gmail.com>
2011-05-30 14:15:36 -0300

    Refactored Greek class and added a greek letter. More to come.

--- lib/namenumber.rb
+++ lib/namenumber.rb
@@ -1,8 +1,68 @@
+# encoding: UTF-8
+
 module NameNumber
   
   class Language
-    def getNumber; 0; end
-    def nameNumber; false; end
+    
+    def number letter
+      return @letter_number[letter.to_sym] if @letter_number.has_key? letter.to_sym
+      0
+    end
+    
+    def possible_number letter
+      result = []
+      result << number(letter)
+      result << number(letter+letter) if number(letter+letter) != 0
+      result
+    end
+    
+    def by_phoneme string
+      string_array = string.split //
+      result = []
+      string_array.each_with_index { |v,i|
+        possible_phoneme = v + string_array[i+1].to_s
+
+        if @letter_number.has_key? possible_phoneme.to_sym
+          string_array.delete_at(i+1)
+          result << possible_phoneme
+        else
+          result << v
+        end
+      }
+      result
+    end
+    
+    def name_number word = ""
+      return false if word.empty?
+      sum = [0]
+      by_phoneme(word).each{ |l|
+        number = possible_number(l)
+        buffer = []
+        if number.count == 1
+          sum.collect! { |s|
+            s += number.first
+          }
+        else
+          number.each { |n|
+            buffer << n
+          }
+        end
+
+        unless buffer.empty?
+          sum_temp = sum.collect { |s| s }
+
+          sum_temp.each_with_index { |v, i|
+            sum[i] = false
+            buffer.each { |b|
+              sum << v + b
+            }
+          }
+          sum.delete_if { |s| s == false }
+        end
+      }
+      sum.sort!
+    end
+    
   end
   
 end
--- test/lib/Letter/test_greek.rb
+++ test/lib/Letter/test_greek.rb
@@ -1,72 +1,80 @@
+# encoding: UTF-8
+
 require "test/unit"
 require "namenumber/greek"
 
 class TestGreek < Test::Unit::TestCase
   
+  def setup
+    @greek = NameNumber::Greek.new
+  end
+  
   def test_greek_numbers
-    assert_equal 1, NameNumber::Greek::number("a")
-    assert_equal 2, NameNumber::Greek::number("b")
-    assert_equal 3, NameNumber::Greek::number("g")
-    assert_equal 4, NameNumber::Greek::number("d")
-    assert_equal 5, NameNumber::Greek::number("e")
-    assert_equal 6, NameNumber::Greek::number("digamma") # doesn't exist anymore
-    assert_equal 7, NameNumber::Greek::number("z")
-    assert_equal 8, NameNumber::Greek::number("ee")
-    assert_equal 9, NameNumber::Greek::number("th")
-#    assert_equal 9, NameNumber::Greek::number("t")
-    assert_equal 10, NameNumber::Greek::number("i")
-    assert_equal 20, NameNumber::Greek::number("k")
-    assert_equal 20, NameNumber::Greek::number("c")
-    assert_equal 30, NameNumber::Greek::number("l")
-    assert_equal 40, NameNumber::Greek::number("m")
-    assert_equal 50, NameNumber::Greek::number("n")
-    assert_equal 60, NameNumber::Greek::number("x")
-    assert_equal 60, NameNumber::Greek::number("sh") # equivalent to x sound
-    assert_equal 70, NameNumber::Greek::number("o")
-    assert_equal 80, NameNumber::Greek::number("p")
-    assert_equal 90, NameNumber::Greek::number("koppa") # koppa letter doesn't exist anymore
-    assert_equal 100, NameNumber::Greek::number("r")
-    assert_equal 200, NameNumber::Greek::number("s")
-    assert_equal 300, NameNumber::Greek::number("t")
-    assert_equal 400, NameNumber::Greek::number("u")
-    assert_equal 500, NameNumber::Greek::number("f")
-    assert_equal 500, NameNumber::Greek::number("ph")
-    assert_equal 600, NameNumber::Greek::number("ch") # greek name is chi
-    assert_equal 700, NameNumber::Greek::number("ps")
-    assert_equal 800, NameNumber::Greek::number("oo") # long o
-    assert_equal 900, NameNumber::Greek::number("sampi") # sampi letter doesn't exist anymore
+    assert_equal 1, @greek.number("a")
+    assert_equal 2, @greek.number("b")
+    assert_equal 3, @greek.number("g")
+    assert_equal 4, @greek.number("d")
+    assert_equal 5, @greek.number("e")
+    assert_equal 6, @greek.number("digamma") # doesn't exist anymore
+    assert_equal 7, @greek.number("z")
+    assert_equal 8, @greek.number("ee")
+    assert_equal 9, @greek.number("th")
+#    assert_equal 9, @greek.number("t")
+    assert_equal 10, @greek.number("i")
+    assert_equal 20, @greek.number("k")
+    assert_equal 20, @greek.number("c")
+    assert_equal 30, @greek.number("l")
+    assert_equal 40, @greek.number("m")
+    assert_equal 50, @greek.number("n")
+    assert_equal 60, @greek.number("x")
+    assert_equal 60, @greek.number("sh") # equivalent to x sound
+    assert_equal 70, @greek.number("o")
+    assert_equal 80, @greek.number("p")
+    assert_equal 90, @greek.number("koppa") # koppa letter doesn't exist anymore
+    assert_equal 100, @greek.number("r")
+    assert_equal 200, @greek.number("s")
+    assert_equal 300, @greek.number("t")
+    assert_equal 400, @greek.number("u")
+    assert_equal 500, @greek.number("f")
+    assert_equal 500, @greek.number("ph")
+    assert_equal 600, @greek.number("ch") # greek name is chi
+    assert_equal 700, @greek.number("ps")
+    assert_equal 800, @greek.number("oo") # long o
+    assert_equal 900, @greek.number("sampi") # sampi letter doesn't exist anymore
   end
   
   def test_possible_number
-    assert_equal [5, 8],    NameNumber::Greek::possible_number("e")
-    assert_equal [70, 800], NameNumber::Greek::possible_number("o")
-    assert_equal [800],     NameNumber::Greek::possible_number("oo")
-    assert_equal [500],     NameNumber::Greek::possible_number("ph")
+    assert_equal [5, 8],    @greek.possible_number("e")
+    assert_equal [70, 800], @greek.possible_number("o")
+    assert_equal [800],     @greek.possible_number("oo")
+    assert_equal [500],     @greek.possible_number("ph")
   end
   
   def test_by_phoneme
-    assert_equal ["oo", "o", "ph", "ee", "e"],  NameNumber::Greek::by_phoneme("ooopheee")
-    assert_equal ["e", "o", "ee", "ph", "ph"],  NameNumber::Greek::by_phoneme("eoeephph")
-    assert_equal ["n", "e", "r", "oo"],         NameNumber::Greek::by_phoneme("neroo")
+    assert_equal ["oo", "o", "ph", "ee", "e"],  @greek.by_phoneme("ooopheee")
+    assert_equal ["e", "o", "ee", "ph", "ph"],  @greek.by_phoneme("eoeephph")
+    assert_equal ["n", "e", "r", "oo"],         @greek.by_phoneme("neroo")
   end
   
   def test_names_number
-    assert_equal [466, 469, 1196, 1199], NameNumber::Greek::name_number("lateino"), "lateino"
-    assert_equal [955, 958], NameNumber::Greek::name_number("neroo"),     "neroo"
-    assert_equal [372], NameNumber::Greek::name_number("italika"),   "italika"
-    assert_equal [294, 297], NameNumber::Greek::name_number("ekkleesia"), "ekklesia"
-    assert_equal [8],   NameNumber::Greek::name_number("hee"),       "hee"
-    assert_equal [399], NameNumber::Greek::name_number("latinee"),   "latinee"
-    assert_equal [259, 262], NameNumber::Greek::name_number("basileia"),  "basileia"
-    assert_equal [615, 618], NameNumber::Greek::name_number("teit"),      "teit"
+    assert_equal [466, 469, 1196, 1199], @greek.name_number("lateino"), "lateino"
+    assert_equal [955, 958],    @greek.name_number("neroo"),      "neroo"
+    assert_equal [372],         @greek.name_number("italika"),    "italika"
+    assert_equal [294, 297],    @greek.name_number("ekkleesia"),  "ekklesia"
+    assert_equal [8],           @greek.name_number("hee"),        "hee"
+    assert_equal [5,8],         @greek.name_number("he"),         "he"
+    assert_equal [399],         @greek.name_number("latinee"),    "latinee"
+    assert_equal [259, 262],    @greek.name_number("basileia"),   "basileia"
+    assert_equal [615, 618],    @greek.name_number("teit"),       "teit"
+    assert_equal [888, 1618],   @greek.name_number("ieesous"),    "ieesous"
   end
   
   def test_multiple_possibilities
-    assert_equal [5, 8],      NameNumber::Greek::name_number("e"), "e or ee?"
-    assert_equal [15, 18],    NameNumber::Greek::name_number("ei"), "ei"
-    assert_equal [8],  NameNumber::Greek::name_number("ee")
-    assert_equal [75, 78, 805, 808],  NameNumber::Greek::name_number("eo")
-    assert_equal [225, 228, 955, 958],  NameNumber::Greek::name_number("nero")
+    assert_equal [5, 8],                @greek.name_number("e"), "e or ee?"
+    assert_equal [15, 18],              @greek.name_number("ei"), "ei"
+    assert_equal [8],                   @greek.name_number("ee")
+    assert_equal [75, 78, 805, 808],    @greek.name_number("eo")
+    assert_equal [225, 228, 955, 958],  @greek.name_number("nero")
   end
   
 end
